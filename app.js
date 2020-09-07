dc5bb35dc758356a1734657524c9bf2254d9b91c
Salvador Murillo <chavitation@gmail.com>
2020-09-07 15:14:14 -0700

    added logout button

--- app.js
+++ app.js
@@ -83,4 +83,12 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
+$("#log_out").on("click",function(){
+  firebase.auth().signOut().then(function() {
+    window.location = 'https://salvador1790.github.io/cloud9/';
+  }).catch(function(error) {
+    // An error happened.
+  });
+})
+
 // END//
